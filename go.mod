6aa9ad793bdb6e7e2c6afc8ea0efcb5a081da0f4
takehito <fukuda.kento@fukehito.com>
2021-07-25 04:01:26 +0900

    change to print builds

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/takehito/aws-tui
 go 1.16
 
 require (
+	github.com/aws/aws-sdk-go-v2 v1.7.1 // indirect
 	github.com/aws/aws-sdk-go-v2/config v1.5.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs v1.5.1 // indirect
 	github.com/aws/aws-sdk-go-v2/service/codebuild v1.5.1 // indirect
 	github.com/gdamore/tcell/v2 v2.3.11 // indirect
 	github.com/rivo/tview v0.0.0-20210624165335-29d673af0ce2 // indirect
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/codebuild"
 	"github.com/aws/aws-sdk-go-v2/service/codebuild/types"
@@ -13,13 +14,17 @@ import (
 	"github.com/rivo/tview"
 )
 
-var app *tview.Application
+var (
+	app *tview.Application
+	cfg aws.Config
+)
 
 func main() {
 	profile := flag.String("profile", "", "aws profile")
 	flag.Parse()
 
-	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithSharedConfigProfile(*profile))
+	var err error
+	cfg, err = config.LoadDefaultConfig(context.TODO(), config.WithSharedConfigProfile(*profile))
 	if err != nil {
 		panic(err)
 	}
@@ -33,32 +38,9 @@ func main() {
 		panic(err)
 	}
 
-	projects := bgp.Projects
-	table := tview.NewTextView()
-	table.SetBorder(true)
-	table.SetText(fmtProjectDescription(projects[0]))
-	codebuilds := tview.NewList().
-		ShowSecondaryText(false).
-		SetDoneFunc(func() {
-			table.Clear()
-			app.SetFocus(table)
-		}).SetSelectedBackgroundColor(tcell.Color102)
-	for _, v := range bgp.Projects {
-		var desc string
-		if v.Description != nil {
-			desc = *v.Description
-		}
-		codebuilds.AddItem(*v.Name, desc, []rune(*v.Name)[0], func() {})
-	}
-	codebuilds.SetChangedFunc(getBuilds(table, bgp.Projects))
-	codebuilds.SetTitle("codebuild projects").SetBorder(true)
-
-	flex := tview.NewFlex().
-		AddItem(codebuilds, 0, 1, true).
-		AddItem(table, 0, 2, true)
-
+	codebuildView := createCodebuildprojectsView(bgp.Projects)
 	app = tview.NewApplication()
-	app.SetRoot(flex, true)
+	app.SetRoot(codebuildView, true)
 	if err := app.Run(); err != nil {
 		panic(err)
 	}
@@ -68,6 +50,54 @@ func getBuilds(table *tview.TextView, projects []types.Project) func(index int, 
 	return func(index int, mainText string, secondaryText string, shortcut rune) {
 		table.SetText(fmtProjectDescription(projects[index]))
 	}
+}
+
+func createBuildsView(project string) tview.Primitive {
+	cli := codebuild.NewFromConfig(cfg)
+	builds, err := cli.ListBuildsForProject(context.Background(), &codebuild.ListBuildsForProjectInput{
+		ProjectName: &project,
+	})
+	if err != nil {
+		panic(err)
+	}
+	list := tview.NewList().ShowSecondaryText(false)
+	list.SetBorder(true)
+
+	for _, v := range builds.Ids {
+		list.AddItem(v, "", []rune(v)[0], func() {})
+	}
+
+	return tview.NewFlex().
+		AddItem(list, 0, 1, true).
+		AddItem(tview.NewTextView(), 0, 2, true)
+}
+
+func createCodebuildprojectsView(projects []types.Project) tview.Primitive {
+	table := tview.NewTextView()
+	table.SetBorder(true)
+	table.SetText(fmtProjectDescription(projects[0]))
+	codebuilds := tview.NewList().
+		ShowSecondaryText(false).
+		SetDoneFunc(func() {
+			table.Clear()
+			app.SetFocus(table)
+		}).SetSelectedBackgroundColor(tcell.Color102)
+	for _, v := range projects {
+		var desc string
+		if v.Description != nil {
+			desc = *v.Description
+		}
+		codebuilds.AddItem(*v.Name, desc, []rune(*v.Name)[0], func() {})
+	}
+	codebuilds.SetChangedFunc(getBuilds(table, projects))
+	codebuilds.SetSelectedFunc(func(i int, _ string, _ string, _ rune) {
+		app.SetRoot(createBuildsView(*projects[i].Name), true)
+	})
+	codebuilds.SetTitle("codebuild projects").SetBorder(true)
+
+	return tview.NewFlex().
+		AddItem(codebuilds, 0, 1, true).
+		AddItem(table, 0, 2, true)
 }
 
 func fmtProjectDescription(project types.Project) string {
@@ -105,6 +135,14 @@ func fmtProjectDescription(project types.Project) string {
 	}
 	if project.SourceVersion != nil {
 		txt := fmt.Sprintf("source version: %s\n", *project.SourceVersion)
+		text.WriteString(txt)
+	}
+	text.WriteString("log: ")
+	if project.LogsConfig == nil {
+		txt := fmt.Sprintf("%##v\n", project)
+		text.WriteString(txt)
+	} else {
+		txt := fmt.Sprintf("%##v\n", project)
 		text.WriteString(txt)
 	}
 	text.WriteString(fmt.Sprintf("build spec: \n%s\n", *project.Source.Buildspec))
